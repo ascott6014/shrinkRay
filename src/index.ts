aa0d0bf349561f1984d1dbd2af548a1d181712bb
Adam Scott <iamadamone@gmail.com>
2025-07-18 15:01:43 -0500

    Add express-mysql-session for session management and update related configurations

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import './config'; // Load environment variables
 import express, { Express } from 'express';
 import session from 'express-session';
+import MySQLStoreFactory from 'express-mysql-session';
 import { registarUser, login } from './controllers/UserController';
 
 const app: Express = express();
-const { PORT, COOKIE_SECRET } = process.env;
+const { PORT, COOKIE_SECRET, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+
+const MySQLStore = MySQLStoreFactory(session as any);
+const sessionStore = new MySQLStore({
+    host: DB_HOST,
+    user: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+});
 
 app.use(
     session({
-        secret: [COOKIE_SECRET], // MAKE NOTE THAT THIS HAS TO BE AN ARRAY
+        store: sessionStore,
+        secret: COOKIE_SECRET, // MAKE NOTE THAT THIS HAS TO BE AN ARRAY
         cookie: { maxAge: 8 * 60 * 60 * 1000}, // 8 hours
         name: 'session',
         resave: false,
