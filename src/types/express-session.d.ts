9ba71e3ec565c749da6b19af9f7c5362d09d7254
Adam Scott <iamadamone@gmail.com>
2025-07-17 22:38:04 -0500

    Add custom session properties to express-session type definition

--- src/types/express-session.d.ts
+++ src/types/express-session.d.ts
@@ -5,5 +5,13 @@ declare module 'express-session' {
     clearSession(): Promise<void>; // DO NOT MODIFY THIS!
 
     // NOTES: Add your app's custom session properties here:
+    authenticatedUser: {
+      userId: string;
+      isPro: boolean;
+      isAdmin: boolean;
+      username: string;
+    };
+    isLoggedIn: boolean;
+
   }
 }
